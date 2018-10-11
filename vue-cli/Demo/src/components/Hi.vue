3abe739e2a51391fd62d1f62306dc309405aa403
南丞 <Lampxiezi@163.com>
2018-10-11 17:07:11 +0800

    更新

---
+++ vue-cli/Demo/src/components/Hi.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="alert alert-success">
+            <strong>作者: {{ msg.author }} 博客: {{msg.url}}</strong> 
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "Hi",
+  data() {
+    return {
+      msg: {
+          author:'南丞',
+          url:'https://friday-go.cc'
+      }
+    };
+  }
+};
+</script>
+<style>
+</style>
