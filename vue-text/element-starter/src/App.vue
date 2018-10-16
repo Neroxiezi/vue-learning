c17b3b9302d8c95c8594e856626dae9971eb523b
南丞 <Lampxiezi@163.com>
2018-10-16 17:23:16 +0800

    更新

---
+++ vue-text/element-starter/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header><e-nav></e-nav></el-header>
+      <el-main><v-content></v-content></el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import eNav from './components/v-nav'
+import vContent from './components/v-main'
+export default {
+  components:{
+    'eNav':eNav,
+    'vContent':vContent
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-text/element-starter/src/components/v-main.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-tabs type="border-card">
+        <el-tab-pane label="个人信息">
+            <el-container>
+                <el-aside width="200px">Aside</el-aside>
+                <el-main class="el-main-content">Main</el-main>
+            </el-container>
+        </el-tab-pane>
+        <el-tab-pane label="个人博客">个人博客</el-tab-pane>
+        <el-tab-pane label="个人社交">角色管理</el-tab-pane>
+        <el-tab-pane label="联系方式">定时任务补偿</el-tab-pane>
+    </el-tabs>
+</template>
+<script>
+export default {};
+</script>
+<style>
+
+</style>
