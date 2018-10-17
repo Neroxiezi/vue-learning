15562e8042b955bb0b276fe9deaf738ac8bd5240
南丞 <lampxiezi@163.com>
2018-10-17 22:27:35 +0800

    更新

--- vue-text/element-starter/src/components/v-main.vue
+++ vue-text/element-starter/src/components/v-main.vue
@@ -2,8 +2,20 @@
     <el-tabs type="border-card">
         <el-tab-pane label="个人信息">
             <el-container>
-                <el-aside width="200px">Aside</el-aside>
-                <el-main class="el-main-content">Main</el-main>
+                <el-aside class="el-aside-content" width="200px">
+                    <el-rate v-model="value2" show-text></el-rate>
+                </el-aside>
+                <el-main class="el-main-content">
+                    <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>卡片名称</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                    </div>
+                    <div v-for="o in 4" :key="o" class="text item">
+                        {{'列表内容 ' + o }}
+                    </div>
+                    </el-card>
+                </el-main>
             </el-container>
         </el-tab-pane>
         <el-tab-pane label="个人博客">个人博客</el-tab-pane>
@@ -12,8 +24,34 @@
     </el-tabs>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      value1: null,
+      value2: null
+    };
+  }
+};
 </script>
 <style>
+.text {
+  font-size: 14px;
+}
 
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 480px;
+}
 </style>
