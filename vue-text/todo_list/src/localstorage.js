d1132e2dd83ff70c1b12351b803dcea728b0dc97
南丞 <Lampxiezi@163.com>
2018-10-23 12:39:08 +0800

    每天一个

---
+++ vue-text/todo_list/src/localstorage.js
@@ -0,0 +1,13 @@
+const STORAGE_KEY = 'student'
+export default {
+    fetch: function () {
+        return JSON.parse(window.localStorage.getItem(STORAGE_KEY) ||
+            '[]')
+    },
+    save: function (items) {
+        window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
+    },
+    clear: function () {
+        window.localStorage.clear();
+    }
+}
