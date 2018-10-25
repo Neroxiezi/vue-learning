e7a140eaf8d85dfd1bed9458a65416201fdbcbbf
南丞 <Lampxiezi@163.com>
2018-10-25 16:12:14 +0800

    跟新

--- vue-text/vue-admin/src/views/nav1/Table.vue
+++ vue-text/vue-admin/src/views/nav1/Table.vue
@@ -13,14 +13,78 @@
                 </el-form-item>
             </el-form>
         </el-col>
+        <el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
+            <el-table-column type="selection" width="55">
+            </el-table-column>
+            <el-table-column type="index" width="60">
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="120" sortable>
+            </el-table-column>
+            <el-table-column prop="sex" label="性别" :formatter="formatSex" width="100" sortable>
+            </el-table-column>
+            <el-table-column prop="age" label="年龄" width="100" sortable>
+            </el-table-column>
+            <el-table-column prop="birth" label="生日" width="120" sortable>
+            </el-table-column>
+            <el-table-column prop="addr" label="地址" min-width="180" sortable>
+            </el-table-column>
+            <el-table-column label="操作" width="150">
+                <template scope="scope">
+                    <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </section>
 </template>
 <script>
+import {
+  getUserListPage,
+  removeUser,
+  batchRemoveUser,
+  editUser,
+  addUser
+} from "../../api/api";
 export default {
   data() {
-      return {
-          
+    return {
+      filters: {
+        name: ""
+      },
+      users: [],
+      total: 0,
+      page: 1,
+      listLoading: false,
+      sels: [], //列表选中列
+      editFormVisible: false, //编辑界面是否显示
+      editLoading: false,
+      editFormRules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }]
       }
+    };
+  },
+  methods: {
+    getUsers() {
+      let para = {
+        page: this.page,
+        name: this.filters.name
+      };
+      this.listLoading = true;
+      getUserListPage(para).then(res => {
+        console.log(res);
+        this.total = res.data.total;
+        this.users = res.data.users;
+        this.listLoading = false;
+      });
+    },
+    formatSex: function(row, column) {
+      return row.sex == 1 ? "♂" : row.sex == 0 ? "♀" : "未知";
+    },
+    handleAdd() {},
+    selsChange() {}
+  },
+  mounted() {
+    this.getUsers();
   }
 };
 </script>
