0a5e8f2005365ba74ddca317558a13abec67a7b4
南城 <lampxiezi@163.com>
2019-11-04 12:26:11 +0800

    update

---
+++ vue_demo/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <navbar></navbar>
+</template>
+
+<script>
+import navbar from "@/components/Nav"
+export default {
+  name: 'Home',
+  components: {
+			navbar	
+		}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ vue_demo/src/components/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+<nav class="navbar navbar-expand-md  bg-dark">
+  <a class="navbar-brand  text-white" href="#">PFinal-CMS</a>
+  <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+      aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavId">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li  v-for="nav in nav_list" class="nav-item" >
+      <!-- <span class="nav-item nav-link text-white" href="{{nav.route_address}}">{{nav.title}} <span class="sr-only">(current)</span></a> -->
+        <router-link :to="nav.address" class="nav-link text-white"> {{nav.title}}</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+<script>
+export default {
+    name :"navbar",
+    data() {
+        return {
+            nav_list: [
+                {title:'首页' ,address:'/'},
+                {title:'文章列表' ,address:'/article'},
+            ]
+        }
+        
+    }
+    
+}
+</script>
